7d7c69330f3d747c9e50f71d9060e20814b734d3
Laurent lesage <laurent.lesage51@gmail.com>
2023-12-26 18:50:16 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lesageduweb.github.io',
+    site: 'https://laurent-66.github.io',
   })
